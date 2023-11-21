689948cbcb099ea3e5ec0b404408a821f65f0996
PeteK4 <118457821+PeteK4@users.noreply.github.com>
2023-11-21 06:12:55 +0200

    Muokattu

--- scriptit/animaatiot.js
+++ scriptit/animaatiot.js
@@ -7,18 +7,17 @@ window.addEventListener("DOMContentLoaded", () => {
     loppupiste = 0;
   }
 
-  // Aloituspiste ja elementti näkyviin
+  // Aloituspiste ja elementti näytölle, alkuun täysin läpinäkyvänä ja piilossa näytön yläreunan takana
   pelialue.style.top = `${-pelialue.offsetHeight - 700}px`;
   pelialue.style.opacity = "0";
   pelialue.style.display = "flex";
 
-  // Liike 
+  // Liike ja läpinäkyvyyden muutos erillisinä aikaviiveinä
   setTimeout(() => {
-    // Liike 
     requestAnimationFrame(() => {
       pelialue.style.transition = "top 1s ease-out, opacity 1.3s ease-in-out";
       pelialue.style.top = `${loppupiste}px`;
       pelialue.style.opacity = "1";
     });
-  }, 500); // Aseta tarvittava aikaviive millisekunteina
+  }, 500);
 });
